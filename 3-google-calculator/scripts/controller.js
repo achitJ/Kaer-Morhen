3bd20a7ae11ff757d881ce4571fd00a1fdfd52a1
achitJ <ajain2801@gmail.com>
2022-01-25 17:38:59 +0530

    added validation

--- 3-google-calculator/scripts/controller.js
+++ 3-google-calculator/scripts/controller.js
@@ -2,28 +2,65 @@ import { calculate } from "./model.js";
 
 window.addEventListener('load', bindEvents);
 
-let compute = function() {
+const valid = function(input) {
+
+    if(input == '') {
+
+        return false;
+    }
+
+    let stack = [];
+
+    for(let index = 0; index < input.length; index++) {
+
+        if(input[index] === '(') {
+
+            stack.push(input[index]);
+        }
+
+        else if(input[index] === ')') {
+
+            stack.pop();
+        }
+    }
+
+    if(stack.length === 0) return true;
+    else return false;
+}
+
+const compute = function() {
 
     const inputDiv = document.querySelector('input');
     const input = inputDiv.value;
 
     inputDiv.value = '';
 
-    if(input == '') {
+    if(!valid(input)) {
 
         return alert("Please enter a valid expression");
     }
 
     const calculatedAns = calculate(input);
+
+    if(isNaN(calculatedAns)) {
+
+        return alert("Please enter a valid expression");
+    }
+
     const output = document.querySelector('#output > span');
 
     output.innerText = calculatedAns;
 }
 
-let input = function(event) {
+const input = function(event) {
 
     const inputDiv = document.querySelector('input');
     const key = event.target.innerText;
+
+    const isOperator = function(key) {
+
+        return key === '+' || key === '-' || key === '*' || key === '/';
+    } 
 
     if(key === 'Back') {
 
@@ -31,6 +68,13 @@ let input = function(event) {
     }
 
     else {
+
+        const input = inputDiv.value;
+
+        if(isOperator(key) && input[input.length - 1] === key) {
+
+            return;
+        }
 
         inputDiv.value += key;
     }
