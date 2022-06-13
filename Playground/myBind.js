2ea3cfc56680acacd761a0010e420a152f0a14a5
Achit Jain <44137933+achitJ@users.noreply.github.com>
2022-06-13 20:03:12 +0530

    added extra method

--- Playground/myBind.js
+++ Playground/myBind.js
@@ -16,3 +16,31 @@ Function.prototype.myBind = function (obj, ...args) {
 
 let newFunc = myFunc.myBind(obj, 'a_random_id')
 newFunc('New York') // Jack, a_random_id, New York
+
+//Without using Apply
+
+/*
+
+let obj = {
+  name: 'Jack',
+}
+
+let myFunc = function (id, city) {
+  console.log(`${this.name}, ${id}, ${city}`)
+}
+
+Function.prototype.myBind = function (obj, ...args) {
+  // console.log(this)
+
+  obj._obj = this;
+  return (...newArgs) => {
+    // console.log(this)
+    // this.apply(obj, [...args, ...newArgs])
+    return obj._obj(...args, ...newArgs);
+  }
+}
+
+let newFunc = myFunc.myBind(obj, 'a_random_id')
+newFunc('New York') // Jack, a_random_id, New York
+
+*/
